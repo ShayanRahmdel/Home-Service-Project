100e003a545989cfc29eb9b1329676822ace16af
shayan rahmdel <shayan.rahmdel@gmail.com>
2024-01-06 17:53:46 +0330

    create Customer

---
+++ src/main/java/entity/Customer.java
@@ -0,0 +1,22 @@
+package entity;
+
+import base.entity.BaseUserEntity;
+import entity.enumration.TypeUser;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Setter
+@Getter
+@Inheritance(strategy = InheritanceType.JOINED)
+public class Customer extends BaseUserEntity<Long> {
+
+    @Enumerated(EnumType.STRING)
+    private TypeUser typeUser;
+}
