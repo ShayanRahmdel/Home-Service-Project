bb9c08cc7f3cb2e22dc3cfd16c73cf885c988d97
shayan rahmdel <shayan.rahmdel@gmail.com>
2024-01-06 17:03:29 +0330

    add BaseUserEntity fields

--- src/main/java/base/entity/BaseUserEntity.java
+++ src/main/java/base/entity/BaseUserEntity.java
@@ -1,4 +1,38 @@
 package base.entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.Column;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.validation.constraints.Email;
+import java.time.LocalDate;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
 public class BaseUserEntity {
+    @Id()
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String firstName;
+
+    @Column(nullable = false)
+    private String lastName;
+
+    @Email
+    @Column(unique = true)
+    private String email;
+
+    @Column(length = 8)
+    private String password;
+
+    private LocalDate signUpDate;
 }
