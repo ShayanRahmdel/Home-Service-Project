40cad2c255cfd282997e641afa0350d9973a88eb
shayan rahmdel <shayan.rahmdel@gmail.com>
2024-01-07 12:30:31 +0330

    create AdminEntity

---
+++ src/main/java/entity/Admin.java
@@ -0,0 +1,18 @@
+package entity;
+
+import base.entity.BaseUserEntity;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.Entity;
+import javax.persistence.Inheritance;
+import javax.persistence.InheritanceType;
+
+@Entity
+@Setter
+@Getter
+@Inheritance(strategy = InheritanceType.JOINED)
+public class Admin extends BaseUserEntity<Long> {
+}
