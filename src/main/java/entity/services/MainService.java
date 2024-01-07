a12173a3a1a81bf7c6091b34b332f6d0089f7c39
shayan rahmdel <shayan.rahmdel@gmail.com>
2024-01-07 17:59:07 +0330

    create MainService

---
+++ src/main/java/entity/services/MainService.java
@@ -0,0 +1,14 @@
+package entity.services;
+
+import base.entity.BaseEntity;
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.Entity;
+
+@Entity
+@Setter
+@Getter
+public class MainService extends BaseEntity<Long> {
+
+}
