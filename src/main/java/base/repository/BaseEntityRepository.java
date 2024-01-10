56a198110d3d1ca65ad8a774a1658e6d662afde4
shayan rahmdel <shayan.rahmdel@gmail.com>
2024-01-10 16:59:11 +0330

    add method in BaseEntityRepository

--- src/main/java/base/repository/BaseEntityRepository.java
+++ src/main/java/base/repository/BaseEntityRepository.java
@@ -1,4 +1,25 @@
 package base.repository;
 
-public interface BaseEntityRepository {
+import base.entity.BaseEntity;
+
+import java.io.Serializable;
+import java.util.Collection;
+import java.util.Optional;
+
+public interface BaseEntityRepository<T extends BaseEntity<ID>, ID extends Serializable> {
+
+    T saveOrUpdate(T entity);
+
+    Optional<T> findById(ID id);
+
+    void deleteById(ID id);
+
+    Collection<T> findAll();
+    boolean existsById(ID id);
+
+    void beginTransaction();
+
+    void commitTransaction();
+
+    void rollBack();
 }
