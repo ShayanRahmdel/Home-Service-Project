ecf87c476eeab5e1def03dc2950f44275b9715c3
shayan rahmdel <shayan.rahmdel@gmail.com>
2024-01-10 10:12:59 +0330

    add fileds

--- src/main/java/entity/users/User.java
+++ src/main/java/entity/users/User.java
@@ -1,13 +1,36 @@
 package entity.users;
 
-import base.entity.BaseUserEntity;
+import base.entity.BaseEntity;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-import javax.persistence.Entity;
+import javax.persistence.*;
+import javax.validation.constraints.Email;
+import java.time.LocalDate;
+
 
 @Entity
 @Setter
 @Getter
-public class User extends BaseUserEntity<Long> {
+@Table(name = "users")
+@AllArgsConstructor
+@NoArgsConstructor
+@Inheritance(strategy = InheritanceType.JOINED)
+public class User extends BaseEntity<Integer> {
+    @Column(nullable = false)
+    private String firstName;
+
+    @Column(nullable = false)
+    private String lastName;
+
+    @Email
+    @Column(unique = true)
+    private String email;
+
+    @Column(length = 8)
+    private String password;
+
+    private LocalDate signUpDate;
 }
