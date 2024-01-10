ebd19c3e4e0a43004a45d291115d33d9f12007d3
shayan rahmdel <shayan.rahmdel@gmail.com>
2024-01-10 10:11:08 +0330

    add comment and wallet

--- src/main/java/entity/users/Customer.java
+++ src/main/java/entity/users/Customer.java
@@ -1,6 +1,8 @@
 package entity.users;
 
-import base.entity.BaseUserEntity;
+
+import entity.business.Comment;
+import entity.business.Wallet;
 import entity.enumration.TypeUser;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -8,15 +10,21 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import javax.persistence.*;
+import java.util.List;
 
 @Entity
 @AllArgsConstructor
 @NoArgsConstructor
 @Setter
 @Getter
-@Inheritance(strategy = InheritanceType.JOINED)
 public class Customer extends User {
 
     @Enumerated(EnumType.STRING)
     private TypeUser typeUser;
+
+    @OneToMany(mappedBy = "customer")
+    private List<Comment> comments;
+
+    @OneToOne(cascade = CascadeType.ALL)
+    private Wallet wallet;
 }
