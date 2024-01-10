4bcd42e0badf67f7236678f3ee62bf65c5533126
shayan rahmdel <shayan.rahmdel@gmail.com>
2024-01-10 10:12:17 +0330

    add subduty and WorkSuggestion and wallet

--- src/main/java/entity/users/Expert.java
+++ src/main/java/entity/users/Expert.java
@@ -1,21 +1,24 @@
 package entity.users;
 
-import base.entity.BaseUserEntity;
+import entity.business.Wallet;
+import entity.business.WorkSuggestion;
 import entity.enumration.Confirmation;
 import entity.enumration.TypeUser;
+import entity.business.SubDuty;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import javax.persistence.*;
+import java.util.List;
+import java.util.Set;
 
 @Entity
 @AllArgsConstructor
 @NoArgsConstructor
 @Setter
 @Getter
-@Inheritance(strategy = InheritanceType.JOINED)
-public class Specialist extends User {
+public class Expert extends User {
 
     @Enumerated(EnumType.STRING)
     private TypeUser typeUser;
@@ -25,5 +28,14 @@ public class Specialist extends User {
 
     private Byte image;
 
+    @ManyToMany
+    @JoinTable(name = "expert_subduty")
+    private Set<SubDuty> subDuties;
+
+    @OneToMany( mappedBy = "expert")
+    private List<WorkSuggestion> workSuggestions;
+
+    @OneToOne(cascade = CascadeType.ALL)
+    private Wallet wallet;
 
 }
