8134c7e2eeb87edb2135d8f263a306890e1f1b03
shayan rahmdel <shayan.rahmdel@gmail.com>
2024-01-10 10:12:35 +0330

    create OrderStatus

---
+++ src/main/java/entity/enumration/OrderStatus.java
@@ -0,0 +1,12 @@
+package entity.enumration;
+
+public enum OrderStatus {
+
+    Waiting_Expert_Sugestion,
+    Waiting_Expert_Selection,
+    Waiting_for_the_specialist_to_come,
+    Work_Being,
+    Work_Done,
+    Paid
+
+}
