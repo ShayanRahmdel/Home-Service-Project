a563db131aac76f7f9109e682a2a4b3999d395a1
shayan rahmdel <shayan.rahmdel@gmail.com>
2024-01-10 10:13:17 +0330

    create Wallat

---
+++ src/main/java/entity/business/Wallet.java
@@ -0,0 +1,28 @@
+package entity.business;
+
+import base.entity.BaseEntity;
+import entity.users.Customer;
+import entity.users.Expert;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.Entity;
+import javax.persistence.OneToOne;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Setter
+@Getter
+public class Wallet extends BaseEntity<Integer> {
+
+    private Double amount;
+
+    @OneToOne(mappedBy = "wallet")
+    private Customer customer;
+
+    @OneToOne(mappedBy = "wallet")
+    private Expert expert;
+}
