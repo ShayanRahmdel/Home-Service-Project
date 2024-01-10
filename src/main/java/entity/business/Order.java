b3e346f0335799d2316088987ef3b4ddfffbdaea
shayan rahmdel <shayan.rahmdel@gmail.com>
2024-01-10 10:12:28 +0330

    create order

---
+++ src/main/java/entity/business/Order.java
@@ -0,0 +1,45 @@
+package entity.business;
+
+import base.entity.BaseEntity;
+import entity.enumration.OrderStatus;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+import java.util.Date;
+import java.util.List;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Setter
+@Getter
+@Table(name = "order_table")
+public class Order extends BaseEntity<Integer> {
+
+    private Double proposedPrice;
+
+    private String jobDescription;
+
+    private LocalDate workDate;
+
+    @Temporal(value = TemporalType.TIME)
+    private Date timeDate;
+
+    @Enumerated(EnumType.STRING)
+    private OrderStatus orderStatus;
+
+    @ManyToOne(fetch = FetchType.EAGER)
+    private SubDuty subDuty;
+
+    @OneToOne
+    private Comment comment;
+
+
+    @OneToMany(mappedBy = "order")
+    private List<WorkSuggestion> workSuggestions;
+
+}
