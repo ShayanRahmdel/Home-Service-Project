c8322303c15a6da2823593f8b183b9ee6a7a6a07
shayan rahmdel <shayan.rahmdel@gmail.com>
2024-01-13 17:19:46 +0330

    change type Byte to byte[]

--- src/main/java/entity/users/Expert.java
+++ src/main/java/entity/users/Expert.java
@@ -9,6 +9,7 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+
 import javax.persistence.*;
 import java.util.List;
 import java.util.Set;
@@ -26,16 +27,22 @@ public class Expert extends User {
     @Enumerated(EnumType.STRING)
     private Confirmation confirmation;
 
-    private Byte[] image;
+    private byte[] image;
 
     @ManyToMany
     @JoinTable(name = "expert_subduty")
     private Set<SubDuty> subDuties;
 
-    @OneToMany( mappedBy = "expert")
+    @OneToMany(mappedBy = "expert")
     private List<WorkSuggestion> workSuggestions;
 
     @OneToOne(cascade = CascadeType.ALL)
     private Wallet wallet;
 
+    public void setImage(byte[] image) {
+        if (image.length > 300 * 1024) {
+            throw new IllegalArgumentException("Image size exceeds the limit of 300KB.");
+        }
+        this.image = image;
+    }
 }
