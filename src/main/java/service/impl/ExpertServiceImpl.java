e59370476b45e874f543c0387149cf94bacdb43a
shayan rahmdel <shayan.rahmdel@gmail.com>
2024-01-13 17:20:09 +0330

    add method changepassword

--- src/main/java/service/impl/ExpertServiceImpl.java
+++ src/main/java/service/impl/ExpertServiceImpl.java
@@ -2,12 +2,18 @@ package service.impl;
 
 import base.service.impl.BaseEntityServiceImpl;
 import entity.business.Comment;
+import entity.enumration.Confirmation;
+import entity.enumration.TypeUser;
 import entity.users.Expert;
 import repository.CommentRepository;
 import repository.ExpertRepository;
 import service.CommentService;
 import service.ExpertService;
 import util.Validate;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
 
 public class ExpertServiceImpl extends BaseEntityServiceImpl<Expert,Integer, ExpertRepository> implements ExpertService {
     public ExpertServiceImpl(ExpertRepository repository) {
@@ -19,12 +25,12 @@ public class ExpertServiceImpl extends BaseEntityServiceImpl<Expert,Integer, Exp
         if (Validate.nameValidation(expert.getFirstName()) &&
                 Validate.nameValidation(expert.getLastName()) &&
                 Validate.emailValidation(expert.getEmail()) &&
-                Validate.passwordValidation(expert.getPassword())){
+                Validate.passwordValidation(expert.getPassword())) {
+            expert.setConfirmation(Confirmation.New);
+            repository.saveOrUpdate(expert);
+            return "successfully signUp";
 
         }
+        return "failed to signUp";
     }
-
-
-
-
 }
