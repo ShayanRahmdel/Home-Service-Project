b9597747342c5c423e09e70f980a1118f7784c55
shayan rahmdel <shayan.rahmdel@gmail.com>
2024-01-13 17:18:34 +0330

    Create admin

--- src/main/java/entity/users/Admin.java
+++ src/main/java/entity/users/Admin.java
@@ -1,18 +1,13 @@
 package entity.users;
 
-import base.entity.BaseUserEntity;
-import lombok.AllArgsConstructor;
+
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import javax.persistence.Entity;
-import javax.persistence.Inheritance;
-import javax.persistence.InheritanceType;
 
 @Entity
-@Setter
 @Getter
-@Inheritance(strategy = InheritanceType.JOINED)
-public class Admin extends BaseUserEntity<Long> {
+@Setter
+public class Admin extends User {
 }
