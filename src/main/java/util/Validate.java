d50ad7cf896b6522d50c5b7b124d2afd3ef24bee
shayan rahmdel <shayan.rahmdel@gmail.com>
2024-01-13 11:15:56 +0330

    add method for password and name validation

--- src/main/java/util/Validate.java
+++ src/main/java/util/Validate.java
@@ -1,8 +1,15 @@
 package util;
 
+import javax.validation.Validator;
+import javax.validation.ValidatorFactory;
+import java.util.Scanner;
 import java.util.regex.Pattern;
 
 public class Validate {
+    private static Scanner input = new Scanner(System.in);
+    public static ValidatorFactory validatorFactory = javax.validation.Validation.buildDefaultValidatorFactory();
+
+    public static Validator validator = validatorFactory.getValidator();
 
     public static Boolean isValidPassword(String password){
         Pattern pattern = Pattern.compile("^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@$!%*?&])[A-Za-z\\d@$!%*?&]{8}$");
@@ -13,4 +20,27 @@ public class Validate {
         return password.matches(pattern.pattern());
     }
 
+    public static String passwordValidation() {
+        String password="";
+        boolean flag = true;
+        while (flag){
+            password = input.next();
+            if (Validate.isValidPassword(password)){
+                flag = false;
+            }else System.out.println("enter valid password");
+        }
+        return password;
+    }
+
+    public static String nameValidation() {
+        String name="";
+        boolean flag = true;
+        while (flag){
+            name = input.next();
+            if (Validate.isValidName(name)){
+                flag = false;
+            }else System.out.println("enter valid password");
+        }
+        return name;
+    }
 }
