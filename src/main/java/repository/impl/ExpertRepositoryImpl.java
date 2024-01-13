3a21f5b43f6bdf0f9495741f9794367319ee4dea
shayan rahmdel <shayan.rahmdel@gmail.com>
2024-01-13 17:20:06 +0330

    add method changepassword

--- src/main/java/repository/impl/ExpertRepositoryImpl.java
+++ src/main/java/repository/impl/ExpertRepositoryImpl.java
@@ -5,6 +5,7 @@ import entity.users.Expert;
 import repository.ExpertRepository;
 
 import javax.persistence.EntityManager;
+import javax.persistence.Query;
 
 public class ExpertRepositoryImpl extends BaseEntityRepositoryImpl<Expert,Integer> implements ExpertRepository {
     public ExpertRepositoryImpl(EntityManager entityManager) {
@@ -14,5 +15,23 @@ public class ExpertRepositoryImpl extends BaseEntityRepositoryImpl<Expert,Intege
     @Override
     public Class<Expert> getEntityClass() {
         return Expert.class;
+    }
+
+    @Override
+    public void changePassword(String email, String oldPassword, String newPassword) {
+        String hql = "UPDATE Expert e set e.password= :newPassword WHERE e.email= :email and e.password= :oldPassword";
+        try {
+            beginTransaction();
+            Query query = entityManager.createQuery(hql);
+            query.setParameter("newPassword",newPassword);
+            query.setParameter("oldPassword",oldPassword);
+            query.setParameter("email",email);
+            query.executeUpdate();
+            commitTransaction();
+        }catch (IllegalArgumentException e){
+            System.out.println("something went wrong");
+            rollBack();
+        }
+
     }
 }
