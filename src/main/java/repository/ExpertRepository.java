0992cc24654fab2c27f08e1f40817bdc05503f49
shayan rahmdel <shayan.rahmdel@gmail.com>
2024-01-13 17:19:59 +0330

    add method changepassword

--- src/main/java/repository/ExpertRepository.java
+++ src/main/java/repository/ExpertRepository.java
@@ -4,4 +4,6 @@ import base.repository.BaseEntityRepository;
 import entity.users.Expert;
 
 public interface ExpertRepository extends BaseEntityRepository<Expert,Integer> {
+
+    void changePassword(String email, String oldPassword,String newPassword);
 }
