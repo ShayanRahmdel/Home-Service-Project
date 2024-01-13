e445f6676ebb7b9128294f0cc2123f49404a9da3
shayan rahmdel <shayan.rahmdel@gmail.com>
2024-01-13 17:18:41 +0330

    Create AdminRepository

---
+++ src/main/java/repository/AdminRepository.java
@@ -0,0 +1,8 @@
+package repository;
+
+import base.repository.BaseEntityRepository;
+import entity.users.Admin;
+
+public interface AdminRepository extends BaseEntityRepository<Admin,Integer> {
+
+}
