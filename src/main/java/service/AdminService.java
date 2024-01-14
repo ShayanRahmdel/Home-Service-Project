6e9b8f410cb04df93d03cd7c679ff373f40f8805
shayan rahmdel <shayan.rahmdel@gmail.com>
2024-01-14 12:40:03 +0330

    add methods createDutyCategory , createSubDuty,seeAllCustomer , seeAllExpert ,SeeAllSubDuty,SeeAllDutyCategory , addExpertInSubDuty

--- src/main/java/service/AdminService.java
+++ src/main/java/service/AdminService.java
@@ -2,14 +2,26 @@ package service;
 
 import base.service.BaseEntityService;
 import entity.business.DutyCategory;
+import entity.business.SubDuty;
 import entity.users.Admin;
 import entity.users.Customer;
+import entity.users.Expert;
 
 import java.util.List;
 
 public interface AdminService extends BaseEntityService<Admin,Integer> {
 
-    String createDutyCategory(DutyCategory dutyCategory);
+    DutyCategory createDutyCategory(DutyCategory dutyCategory);
+
+    SubDuty createSubDuty(SubDuty subDuty,Integer category);
 
     List<Customer> seeAllCustomer();
+
+    List<Expert> seeAllExpert();
+
+    List<DutyCategory> seeAllDutyCategories();
+    List<SubDuty> seeAllSubDuty();
+
+    void addExpertInSubDuty(Expert expert);
+
 }
