9990072071132429c88fdebff03eebf4b1af134b
shayan rahmdel <shayan.rahmdel@gmail.com>
2024-01-15 15:59:49 +0330

    add seeSubDutyByCategory

--- src/main/java/service/SubDutyService.java
+++ src/main/java/service/SubDutyService.java
@@ -4,5 +4,9 @@ import base.service.BaseEntityService;
 import entity.business.SubDuty;
 import entity.users.Customer;
 
+import java.util.List;
+
 public interface SubDutyService extends BaseEntityService<SubDuty,Integer> {
+
+    List<SubDuty> seeSubDutyByCategory(Integer category);
 }
