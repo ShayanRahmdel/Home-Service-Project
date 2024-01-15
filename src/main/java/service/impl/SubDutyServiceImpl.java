ecca541446049630a5deb4eb8b45e4edc5f53edb
shayan rahmdel <shayan.rahmdel@gmail.com>
2024-01-15 15:59:55 +0330

    impl seeSubDutyByCategory

--- src/main/java/service/impl/SubDutyServiceImpl.java
+++ src/main/java/service/impl/SubDutyServiceImpl.java
@@ -8,9 +8,27 @@ import repository.SubDutyRepository;
 import service.CommentService;
 import service.SubDutyService;
 
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+import java.util.Objects;
+
 public class SubDutyServiceImpl extends BaseEntityServiceImpl<SubDuty, Integer, SubDutyRepository> implements SubDutyService {
 
     public SubDutyServiceImpl(SubDutyRepository repository) {
         super(repository);
+    }
+
+    @Override
+    public List<SubDuty> seeSubDutyByCategory(Integer category) {
+        List<SubDuty> subDutyList = new ArrayList<>();
+        Collection<SubDuty> allSubDuty = repository.findAll();
+        for (SubDuty subDuty : allSubDuty){
+            if (Objects.equals(subDuty.getDutyCategory().getId(), category)){
+                subDutyList.add(subDuty);
+            }
+
+        }
+        return subDutyList;
     }
 }
