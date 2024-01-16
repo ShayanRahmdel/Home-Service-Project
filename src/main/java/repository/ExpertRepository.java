38f00bef99be838a6b51cf15a905a8051ce5f4ed
shayan rahmdel <shayan.rahmdel@gmail.com>
2024-01-16 14:55:08 +0330

    add seeOrders

--- src/main/java/repository/ExpertRepository.java
+++ src/main/java/repository/ExpertRepository.java
@@ -1,9 +1,14 @@
 package repository;
 
 import base.repository.BaseEntityRepository;
+import entity.business.Order;
 import entity.users.Expert;
+
+import java.util.List;
 
 public interface ExpertRepository extends BaseEntityRepository<Expert,Integer> {
 
     void changePassword(String email, String oldPassword,String newPassword);
+
+    List<Order> seeOrder(Integer expertId);
 }
