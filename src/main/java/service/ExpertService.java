26cfe127ff6852f3110c3b3a62ce98cdf071d89a
shayan rahmdel <shayan.rahmdel@gmail.com>
2024-01-16 14:55:34 +0330

    add seeOrders

--- src/main/java/service/ExpertService.java
+++ src/main/java/service/ExpertService.java
@@ -1,10 +1,16 @@
 package service;
 
 import base.service.BaseEntityService;
+import entity.business.Order;
 import entity.users.Customer;
 import entity.users.Expert;
+
+import java.util.List;
 
 public interface ExpertService extends BaseEntityService<Expert,Integer> {
 
     String signUp(Expert expert);
+
+    List<Order> seeOrder(Integer expertId);
+
 }
